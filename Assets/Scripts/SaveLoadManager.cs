df86d65295e27acefc25056d3099448b7bdfd011
Amirreza Amouie <aamouie01@gmail.com>
2021-02-03 02:15:21 +0330

    add save n load manager class

---
+++ Assets/Scripts/SaveLoadManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveLoadManager
+{
+    bool isJetpackActive;
+    float jetpackCharge;
+
+    bool isTabletActive;
+    float tabletCharge;
+
+    (float, float) playerPosition;
+
+    // int = index of the gameobject inside the group of filter gameobjects. only includes the active ones.
+    // so when saving, only the active ones are considered and when loading, some active objects by default get deactivated to
+    // recreate the state before saving.
+    List<int> nofilterDeactivatedObjects; 
+    List<int> redDeactivatedObjects;
+    List<int> greenDeactivatedObjects;
+    List<int> blueDeactivatedObjects;
+
+}
