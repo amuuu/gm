f31cdde4aee9fc1ba95efeadc11dad9b626f0dab
Amirreza Amouie <aamouie01@gmail.com>
2021-02-02 19:18:04 +0330

    seperate movement logic from view and config data

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,38 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed;
-    public float movementTime;
-
+    private MovementView movementView;
+    public MovementConfigData movementConfig;
+    
     private Vector2 newPosition;
-    private SpriteRenderer spriteRenderer;
- 
+
     void Start()
     {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        newPosition = transform.position;
+        movementView = new MovementView(gameObject);
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            newPosition = new Vector2(transform.position.x + movementSpeed, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+            newPosition = new Vector2(transform.position.x + movementConfig.movementSpeed, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, newPosition, Time.deltaTime * movementConfig.movementTime);
 
-            spriteRenderer.flipX = false;
+            movementView.SetSpriteRendererFlipX(false);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            newPosition = new Vector2(transform.position.x + -movementSpeed, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+            newPosition = new Vector2(transform.position.x + -movementConfig.movementSpeed, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, newPosition, Time.deltaTime * movementConfig.movementTime);
 
-            spriteRenderer.flipX = true;
+            movementView.SetSpriteRendererFlipX(true);
+
         }
     }
 }
 
+public class MovementView
+{
+    private SpriteRenderer spriteRenderer;
+    private GameObject gameObject;
+
+    public MovementView(GameObject obj)
+    {
+        gameObject = obj;
+        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void SetSpriteRendererFlipX(bool b)
+    {
+        spriteRenderer.flipX = b;
+    }
+}
 
 
+[CreateAssetMenu(menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class MovementConfigData : ScriptableObject
+{
+    public float movementSpeed;
+    public float movementTime;
+}
