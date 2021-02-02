cc8085d89cca5d09d9f4b33e03d37a74b4f4fb59
Amirreza Amouie <aamouie01@gmail.com>
2021-02-02 20:36:29 +0330

    refactor the whole goggle filter feature

--- Assets/Scripts/PlayerGoggles.cs
+++ Assets/Scripts/PlayerGoggles.cs
@@ -7,124 +7,159 @@ using UnityEngine.Rendering.PostProcessing;
 
 // PP = Post Processing
 
+struct PPParameter
+{
+    public Color color;
+    public float exposure;
+}
+
+struct GoggleFilter
+{
+    
+    public string name;
+    public GameObject gameObjects;
+    public PPParameter parameters;
+
+    public void Initialize(string name, GameObject gameObjects, Color color, float exposure)
+    {
+        this.name = name;
+        
+        this.gameObjects = new GameObject();
+        this.gameObjects = gameObjects;
+        
+        this.parameters = new PPParameter();
+        this.parameters.color = new Color();
+        this.parameters.color = color;
+        this.parameters.exposure = exposure;
+    }
+}
+
 public class PlayerGoggles : MonoBehaviour
 {
-    private string[] filters = { "NoFilter", "Red", "Green", "Blue" };
-    private GameObject[] filtersObjectsArray;
-    private Color[] filterColorsForPP;
-
+    private GoggleFilter[] goggleFilters;
+    private GoggleFilter currentFilter;
+    private int filtersNum;
     private int filterCursor;
+
+    private GogglesView goggleView;
+    private GameObject mainCamera;
+
+
     void Start()
     {
-        filterCursor = 0;
+        goggleView = new GogglesView();
         
-        InitFilterColorsForPP();
-        InitFilterObjectsArray();
-
-        ActivateFilter("NoFilter");
-        UpdateTextUI("NoFilter");
-        UpdatePostProcessingEffect("NoFilter");
+        InitCamera();
+        
+        filtersNum = 4;
+        filterCursor = 0;
+        InitGoggleFilters();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            string nextFilter = GetNextFilterName();
-            UpdateTextUI(nextFilter);
-            ActivateFilter(nextFilter);
-            UpdatePostProcessingEffect(nextFilter);
+            GoToNextFilterCursor();
+
+            currentFilter = goggleFilters[filterCursor];
+            
+            goggleView.UpdateGoggleFilterTextUI(currentFilter.name);
+            
+            ActivateCurrentFilterGameObjects();
+            
+            UpdatePostProcessingEffect();
         }
     }
 
-    void UpdateTextUI(string filterName)
+    private void UpdatePostProcessingEffect()
     {
-        GameObject filterTextUI = GameObject.FindGameObjectsWithTag("GoggleFilterText")[0];
+        PostProcessVolume ppv = mainCamera.GetComponent<PostProcessVolume>();
+        ColorGrading colorGrading;
+        
+        ppv.profile.TryGetSettings(out colorGrading);
 
-        var filterText = filterTextUI.GetComponent<Text>();
+        colorGrading.colorFilter.value = currentFilter.parameters.color;
+        colorGrading.postExposure.value = currentFilter.parameters.exposure;
+    }
+
+
+    private void GoToNextFilterCursor()
+    {
+        filterCursor++;
+        
+        if (filterCursor == filtersNum)
+            filterCursor = 0;
+    }
+
+    private void InitGoggleFilters()
+    {
+        goggleFilters = new GoggleFilter[filtersNum];
+
+        // No Filter
+        GoggleFilter nofilter = new GoggleFilter();
+        nofilter.Initialize("NoFilter",
+                            GameObject.FindGameObjectsWithTag("NoFilter")[0],
+                            new Color(255.0f, 255.0f, 255.0f, 1.0f),
+                            -8f);
+        goggleFilters[0] = nofilter;
+
+        // Red
+        GoggleFilter red = new GoggleFilter();
+        red.Initialize("Red",
+                            GameObject.FindGameObjectsWithTag("Red")[0],
+                            new Color(255.0f, 45.0f, 51.0f, 1.0f),
+                            -6f);
+        goggleFilters[1] = red;
+
+        // Green
+        GoggleFilter green = new GoggleFilter();
+        green.Initialize("Green",
+                            GameObject.FindGameObjectsWithTag("Green")[0],
+                            new Color(98.0f, 255.0f, 83.0f, 1.0f),
+                            -6f);
+        goggleFilters[2] = green;
+
+        // Blue
+        GoggleFilter blue = new GoggleFilter();
+        blue.Initialize("Blue",
+                        GameObject.FindGameObjectsWithTag("Blue")[0],
+                        new Color(9.0f, 80.0f, 255.0f, 1.0f),
+                        -6f);
+        goggleFilters[3] = blue;
+
+    }
+
+    private void ActivateCurrentFilterGameObjects()
+    {
+        goggleFilters[filterCursor].gameObjects.SetActive(true);
+
+        for (int i=0; i<filtersNum; i++)
+            if (i != filterCursor)
+                goggleFilters[i].gameObjects.SetActive(false);
+    }
+
+    private void InitCamera()
+    {
+        mainCamera = gameObject.transform.GetChild(0).gameObject;
+        if (mainCamera.tag != "MainCamera")
+            return;
+    }
+}
+
+class GogglesView
+{
+    GameObject textUI;
+    
+    public GogglesView()
+    {
+        textUI = GameObject.FindGameObjectsWithTag("GoggleFilterText")[0];
+    }
+
+    public void UpdateGoggleFilterTextUI(string filterName)
+    {
+        var filterText = textUI.GetComponent<Text>();
 
         filterText.text = "GOGGLE FILTER: " + filterName;
     }
-
-    private string GetNextFilterName()
-    {
-        filterCursor++;
-        if (filterCursor == filters.Length)
-            filterCursor = 0;
-        return filters[filterCursor];
-    }
-
-    private void ActivateFilter(string filterName)
-    {
-        filtersObjectsArray[filterNameToIndex(filterName)].SetActive(true);
-
-        foreach (string filter in filters)
-        {
-            if (filter != filterName)
-            {
-                filtersObjectsArray[filterNameToIndex(filter)].SetActive(false);
-            }
-        }
-    }
-
-    private void InitFilterObjectsArray()
-    {
-        filtersObjectsArray = new GameObject[4];
-
-        int index = 0;
-        foreach (string filter in filters)
-        {
-            filtersObjectsArray[index] = GameObject.FindGameObjectsWithTag(filter)[0];
-            index++;
-        }
-    }
-
-    private int filterNameToIndex(string filterName)
-    {
-        return Array.IndexOf(filters, filterName); // this can be optimizaed.
-    }
-
-    private void UpdatePostProcessingEffect(string filterName)
-    {
-        GameObject camera = gameObject.transform.GetChild(0).gameObject;
-        if (camera.tag != "MainCamera")
-            return;
-
-        PostProcessVolume ppv = camera.GetComponent<PostProcessVolume>();
-        ColorGrading colorGrading;
-        ppv.profile.TryGetSettings(out colorGrading);
-
-        switch (filterName)
-        {
-            case "NoFilter":
-                colorGrading.colorFilter.value = filterColorsForPP[0];
-                colorGrading.postExposure.value = -8f;
-                break;
-            case "Red":
-                colorGrading.colorFilter.value = filterColorsForPP[1];
-                colorGrading.postExposure.value = -6f;
-                break;
-            case "Green":
-                colorGrading.colorFilter.value = filterColorsForPP[2];
-                colorGrading.postExposure.value = -6f;
-                break;
-            case "Blue":
-                colorGrading.colorFilter.value = filterColorsForPP[3];
-                colorGrading.postExposure.value = -6f;
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void InitFilterColorsForPP()
-    {
-        filterColorsForPP = new Color[4];
-
-        filterColorsForPP[0] = new Color(255.0f, 255.0f, 255.0f, 1.0f); // Pure white tint
-        filterColorsForPP[1] = new Color(255.0f, 45.0f, 51.0f, 1.0f); // Red tint
-        filterColorsForPP[2] = new Color(98.0f, 255.0f, 83.0f, 1.0f); // Green tint
-        filterColorsForPP[3] = new Color(9.0f, 80.0f, 255.0f, 1.0f); // Blue tint
-    }
-
 }
